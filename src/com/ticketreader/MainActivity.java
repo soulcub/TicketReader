514f44949388ac804da4bd854de132abb7dfd3e9
soulcub <soulcub@gmail.com>
2012-07-22 19:50:15 +0300

    Added camera module + image view

--- src/com/ticketreader/MainActivity.java
+++ src/com/ticketreader/MainActivity.java
@@ -1,17 +1,37 @@
 package com.ticketreader;
 
-import android.os.Bundle;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	private static final int REQUEST_CODE = 1;
+	private Bitmap bitmap;
+	private ImageView imageView;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.result);
+        
+        final Button button = (Button) findViewById(R.id.run_button);
+        button.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+            	pickImage(v);
+            }
+        });
     }
 
     @Override
@@ -19,6 +39,32 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-
     
+	public void pickImage(View View) {
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		startActivityForResult(intent, REQUEST_CODE);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK)
+			try {
+				// We need to recyle unused bitmaps
+				if (bitmap != null) {
+					bitmap.recycle();
+				}
+				InputStream stream = getContentResolver().openInputStream(data.getData());
+				bitmap = BitmapFactory.decodeStream(stream);
+				stream.close();
+				imageView.setImageBitmap(bitmap);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 }
